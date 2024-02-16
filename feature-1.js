6ed8d98810f206f069f1e9e9f005d86fa6583df0
Andrew <thum14252@gmail.com>
2024-02-16 15:58:49 +0630

    added feature1

--- feature-1.js
+++ feature-1.js
@@ -1,8 +1,8 @@
 function add() {
-    const h3 = document.getElementById("id");
+    const h3 = document.getElementById("h3");
     let number = 1;
     number++
     console.log(number);
-    h3.innerHTML = number;
+    h3.innerText = number;
 }
 add();
